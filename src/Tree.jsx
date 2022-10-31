87f53d98924f7ede379b65341a4089d5e1ed82d6
Annmary23 <anne1oshiobughie@gmail.com>
2022-10-31 19:07:54 +0100

    first hng commit

--- src/Tree.jsx
+++ src/Tree.jsx
@@ -1,4 +1,4 @@
-import profileLogo from './assets/supreme.png'
+import profileLogo from './assets/annimage.png'
 import shareButton from './assets/share-button.png'
 import shareIcon from './assets/share-icon.png'
 import slackIcon from './assets/slack.png'
@@ -9,45 +9,45 @@ import "./Tree.css";
 
 const Tree = ( ) => {
     return (
-        <div className="tree">
+        <div className="hng-tree">
             <div className='body'>
-                <div className="top-section">
+                <div className="main">
                     <div>
                         <img src={profileLogo} alt="profile image" id="profile__img" width="88px" height="88px"/>
                     </div>
                     <div className="share_button">
                         <picture>
                             <source media="(max-width: 400px)" srcset={shareIcon}/>
-                            <img src={shareButton} alt="avatar share button"/>
+                            <img src={shareButton} alt="share button avatar"/>
                         </picture>
                     </div>
                 </div>
 
-                <div id="twitter">Black Dev</div>
-                <div id="slack">BlackDev</div>
+                <div id="twitter">Sexy_Programmer</div>
+                <div id="slack">Annie_Dev</div>
 
-                <a href="https://twitter.com/BlackDev112" id="twitter__link" target="_blank" rel="noreferrer nooppener">Twitter Link</a>
+                <a href="https://twitter.com/crayfish_bae" id="twitter__link" target="_blank" rel="noreferrer nooppener">Twitter Link</a>
                 <a href="https://training.zuri.team/" id="btn__zuri" target="_blank" rel="noreferrer nooppener">Zuri Team</a>
-                <a href="https://books.zuri.team/" id="books" target="_blank" rel="noreferrer nooppener" title="This is where you find books about design and coding">Zuri Books</a>
-                <a href="https://books.zuri.team/python-for-beginners?ref_id=BlackDev" id="book__python" target="_blank" rel="noreferrer nooppener">Python Books</a>
-                <a href="https://background.zuri.team/" id="pitch" target="_blank" rel="noreferrer nooppener" title="Connect you with certified programmers">Background Check for Coders</a>
-                <a href="https://books.zuri.team/design-rules" id="book__design" target="_blank" rel="noreferrer nooppener" title='Become a design master, for free'>Design Books</a>
-                <div className='social_section'>
+                <a href="https://books.zuri.team/" id="books" target="_blank" rel="noreferrer nooppener" title="Find your coding and design books here">Zuri Books</a>
+                <a href="https://books.zuri.team/python-for-beginners?ref_id=Annie_Dev" id="book__python" target="_blank" rel="noreferrer nooppener">Python Books</a>
+                <a href="https://background.zuri.team/" id="pitch" target="_blank" rel="noreferrer nooppener" title="Linking you with world class developers">Background Check for Coders</a>
+                <a href="https://books.zuri.team/design-rules" id="book__design" target="_blank" rel="noreferrer nooppener" title='Learn design from zero to hero for free'>Design Books</a>
+                <div className='social__section'>
                     <div><img src={slackIcon} alt="slack logo"/></div>
-                    <a href='https://github.com/samuel99Oshiobughie'><img src={githubIcon} alt=" github logo"/></a>
+                    <a href='https://github.com/Annmary23'><img src={githubIcon} alt=" github logo"/></a>
                 </div>
                 <footer>        
-                    <div className='zuriLogo'>
+                    <div className='Logo-zuri'>
                         <h2>
                             Zuri 
                             <span></span>
                                 Internship
                         </h2>
                     </div>
-                    <div className='zuriText'>
+                    <div className='Text-zuri'>
                         <p>HNG Internship 9 Frontend Task</p>
                     </div>
-                    <div className='i4gLogo'> 
+                    <div className='Logo-i4g'> 
                     <img src={I4G} alt="I4G logo" width="100px"/>
                     </div>  
                 </footer>
